47c31c85ea33ad0ecfe19f8b1b5f3daafe760d7f
tiwariak31 <43091126+tiwariak31@users.noreply.github.com>
2020-04-25 18:05:38 +0530

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# game-areana
-Created with CodeSandbox
---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppRoutingModule } from "./app.routing.module";
+import { AppComponent } from "./app.component";
+import { CoreModule } from "./core/core.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { SharedModule } from "./shared/shared.module";
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    BrowserAnimationsModule,
+    SharedModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
